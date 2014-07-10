fa1cc99648665990cffd1a513b4bf39d8ab995d1
morgankeller <morganlkeller@gmail.com>
2014-07-10 13:24:20 -0700

    Updated DB

--- db/chimay.sql
+++ db/chimay.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.6
+-- version 3.3.9.2
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jun 25, 2014 at 02:44 PM
--- Server version: 5.5.33
--- PHP Version: 5.5.3
+-- Generation Time: Jul 03, 2014 at 11:22 AM
+-- Server version: 5.5.9
+-- PHP Version: 5.3.6
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -29,26 +28,24 @@ SET time_zone = "+00:00";
 CREATE TABLE `clients` (
   `clientID` int(11) NOT NULL AUTO_INCREMENT,
   `clientName` varchar(255) NOT NULL,
-  `clientShortname` varchar(255) NOT NULL,
   `clientAddress1` varchar(255) NOT NULL,
   `clientAddress2` varchar(255) NOT NULL,
   `clientCity` varchar(255) NOT NULL,
   `clientState` varchar(2) NOT NULL,
   `clientZip` int(5) DEFAULT NULL,
-  `clientContact` varchar(255) NOT NULL,
-  `clientEmail` varchar(255) NOT NULL,
-  `clientCreated` datetime NOT NULL,
+  `contactID` int(11) NOT NULL,
+  `clientCreated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`clientID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
 
 --
 -- Dumping data for table `clients`
 --
 
-INSERT INTO `clients` (`clientID`, `clientName`, `clientShortname`, `clientAddress1`, `clientAddress2`, `clientCity`, `clientState`, `clientZip`, `clientContact`, `clientEmail`, `clientCreated`) VALUES
-(1, 'Grasswalkers, LLC', 'Grasswalkers', '2223 Avenida de la Playa', 'Suite 110', 'La Jolla', 'CA', 92037, 'Shari Farah', 'sfarah@shwx2.com', '2013-08-20 19:52:07'),
-(4, 'Sony Computer Entertainment', 'SCE', '1630 Stewart St', '', 'Santa Monica', 'CA', 90404, 'Aaron Kaufman', 'aaron_kaufman@playstation.sony.com', '2014-03-27 00:00:00'),
-(5, 'Annenberg School for Communication', 'Annenberg', 'Watt Way', '', 'Los Angeles', 'CA', 90007, 'Rachel Hartz', 'rachellhartz@gmail.com', '2014-04-24 00:00:00');
+INSERT INTO `clients` VALUES(1, 'Grasswalkers, LLC', '2223 Avenida de la Playa', 'Suite 110', 'La Jolla', 'CA', 92037, 0, '2013-08-20 19:52:07');
+INSERT INTO `clients` VALUES(4, 'Sony Computer Entertainment', '1630 Stewart St', '', 'Santa Monica', 'CA', 90404, 0, '2014-03-27 00:00:00');
+INSERT INTO `clients` VALUES(5, 'Annenberg School for Communication', 'Watt Way', '', 'Los Angeles', 'CA', 90007, 0, '2014-04-24 00:00:00');
+INSERT INTO `clients` VALUES(6, 'Client Name Test', '123 Test St.', '', 'Los Angeles', 'CA', 90250, 0, '2014-06-25 12:58:10');
 
 -- --------------------------------------------------------
 
@@ -63,6 +60,32 @@ CREATE TABLE `contacts` (
   `contactEmail` varchar(100) NOT NULL,
   PRIMARY KEY (`contactID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `contacts`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `messages`
+--
+
+CREATE TABLE `messages` (
+  `messageID` int(11) NOT NULL AUTO_INCREMENT,
+  `messageCreated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `messageTitle` mediumtext NOT NULL,
+  `messageBody` text NOT NULL,
+  `userID` int(11) NOT NULL,
+  PRIMARY KEY (`messageID`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `messages`
+--
+
+INSERT INTO `messages` VALUES(1, '2014-06-25 11:53:35', 'Test message title', 'This is the content of the message', 1);
 
 -- --------------------------------------------------------
 
@@ -85,9 +108,4 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`userID`, `userName`, `userFirstName`, `userLastName`, `userEmail`, `userPassword`, `userApproved`) VALUES
-(1, 'morgan', 'Morgan', 'Keller', 'morgan@indiebrewco.com', 'morgan', 1);
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+INSERT INTO `users` VALUES(1, 'morgan', 'Morgan', 'Keller', 'morgan@indiebrewco.com', 'morgan', 1);
